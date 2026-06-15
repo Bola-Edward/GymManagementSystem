c18e5ef06566b381d88c5664cd416fe6d365959b
bolaedward <bolaedward29@gmail.com>
2026-06-15 11:16:51 +0300

    Implement Plans read operations (Index and Details)

---
+++ GymManagementSystem/Controllers/PlanController1.cs
@@ -0,0 +1,32 @@
+﻿using GymManagementSystem.Data.Contexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymManagementSystem.Controllers
+{
+    public class PlansController : Controller
+    {
+        public GymDbContext dbContext = new GymDbContext();
+        public async Task<IActionResult> Index()
+        {
+            var plans = await dbContext.Plans.ToListAsync();
+            return View(plans);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            if (id <= 0)
+            {
+                return NotFound();
+            }
+            var plan = await dbContext.Plans.FirstOrDefaultAsync(p => p.Id == id);
+
+            if (plan is null)
+            {
+                return RedirectToAction(nameof(Index)); // return 302 location
+            }
+
+            return View(plan);  // Views/Plans/Details.cshtml
+        }
+    }
+}
