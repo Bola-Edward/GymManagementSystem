1ed4b2720d96e1206090265667a4f601779b72ec
bolaedward <bolaedward29@gmail.com>
2026-06-15 10:30:39 +0300

    Setup GymDbContext and Plan configuration

---
+++ GymManagementSystem/Data/Configuration/PlanConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using GymManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GymManagementSystem.Data.Configuration
+{
+    public class PlanConfiguration : IEntityTypeConfiguration<Plan>
+    {
+        public void Configure(EntityTypeBuilder<Plan> builder)
+        {
+
+            builder.Property(p => p.Name)
+                .HasMaxLength(50);
+
+            builder.Property(p => p.Description)
+                .HasMaxLength(200);
+
+            builder.Property(p => p.Price)
+                .HasPrecision(10, 2);
+
+            builder.ToTable(tb =>
+            {
+                tb.HasCheckConstraint("PlanDurationDays",
+                    "DurationDays BETWEEN 1 AND 365");
+            });
+
+            builder.HasIndex(b => b.Name)
+                .IsUnique();
+        }
+    }
+}
---
+++ GymManagementSystem/Data/Contexts/GymDbContext.cs
@@ -0,0 +1,21 @@
+﻿using GymManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymManagementSystem.Data.Contexts
+{
+    public class GymDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.; Database=GymManagementSystem;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Apply all configurations from the assembly containing GymDbContext
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(GymDbContext).Assembly);
+        }
+
+        public DbSet<Plan> Plans { get; set; }
+    }
+}
