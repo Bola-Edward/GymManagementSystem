388b5dd9b5e703ead79ca16a67a324e8e80b38ab
bolaedward <bolaedward29@gmail.com>
2026-06-20 15:39:14 +0300

    Add repository interfaces and implementations

---
+++ GymManagementSystem.DAL/Repositories/PlanRepository.cs
@@ -0,0 +1,48 @@
+﻿using GymManagementSystem.Data.Contexts;
+using GymManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymManagementSystem.DataAccess.Repositories
+{
+    public class PlanRepository : IPlanRepository
+    {
+        private readonly GymDbContext _dbContext;
+
+        public PlanRepository(GymDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Add(Plan plan)
+        {
+            _dbContext.Add(plan);
+        }
+
+        public void Delete(Plan plan)
+        {
+            _dbContext.Remove(plan);
+        }
+
+        public async Task<IEnumerable<Plan>> GetAllAsync()
+        {
+            return await _dbContext.Plans.ToListAsync();
+        }
+
+        public async Task<Plan?> GetByIdAsync(int id)
+        {
+            return await _dbContext.Plans
+                .FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public void Update(Plan plan)
+        {
+            _dbContext.Update(plan);
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _dbContext.SaveChangesAsync();
+        }
+
+    }
+}
