19da92774f112cc4f26aaedda1d2941eb45d6299
bolaedward <bolaedward29@gmail.com>
2026-06-23 10:16:34 +0300

    Add Address value object

---
+++ GymManagementSystem.DAL/Models/ValueObjects/Address.cs
@@ -0,0 +1,11 @@
+﻿
+namespace GymManagementSystem.DAL.Models.ValueObjects
+{
+    public class Address
+    {
+        public string Street { get; set; } = default!;
+        public string City { get; set; } = default!;
+        public int BuildingNumber { get; set; }
+
+    }
+}
