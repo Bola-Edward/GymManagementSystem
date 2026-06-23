845793cef3264f8b07e9c156634923e24bf2c7b1
bolaedward <bolaedward29@gmail.com>
2026-06-23 09:58:30 +0300

    Add Category entity and configuration

---
+++ GymManagementSystem.DAL/Data/Configuration/CategoryConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using GymManagementSystem.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymManagementSystem.DAL.Data.Configuration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(c => c.Name)
+                 .HasMaxLength(30);
+
+            builder.HasQueryFilter(c => !c.IsDeleted);
+        }
+    }
+}
---
+++ GymManagementSystem.DAL/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Collections.Specialized.BitVector32;
+
+namespace GymManagementSystem.DAL.Models
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; } = default!;
+
+    }
+}
