f14b5f0e69fc661b78bc0aa509fef69f44654eae
bolaedward <bolaedward29@gmail.com>
2026-06-23 10:14:13 +0300

    Add domain enums

---
+++ GymManagementSystem.DAL/Enums/Speciality.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymManagementSystem.DAL.Enums
+{
+    public enum Speciality
+    {
+        PersonalTraining = 1,
+        Yoga = 2,
+        CrossFit = 3,
+        Boxing = 4,
+        Cardio = 5,
+        BodyBuilding = 6,
+    }
+}
