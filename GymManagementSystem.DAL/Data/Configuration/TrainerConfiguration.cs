8cfd74832842c32de4a15bf89065d247b5b5dc66
bolaedward <bolaedward29@gmail.com>
2026-06-23 10:25:39 +0300

    Add Trainer entity and database configuration

---
+++ GymManagementSystem.DAL/Data/Configuration/TrainerConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using GymManagementSystem.DAL.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymManagementSystem.DAL.Data.Configuration
+{
+    public class TrainerConfiguration : UserConfiguration<Trainer>
+    {
+        public override void Configure(EntityTypeBuilder<Trainer> builder)
+        {
+            base.Configure(builder);
+
+            // configuration related to the Trainer entity can be added here if needed
+
+            builder.Property(p => p.Speciality)
+                .HasConversion<string>()
+                .HasMaxLength(30);
+
+        }
+    }
+}
---
+++ GymManagementSystem.DAL/Models/Trainer.cs
@@ -0,0 +1,11 @@
+﻿using GymManagementSystem.DAL.Enums;
+
+namespace GymManagementSystem.DAL.Models
+{
+    public class Trainer : User
+    {
+        public Speciality Speciality { get; set; } = default!;
+        public DateTime HireDate { get; set; }
+
+    }
+}
