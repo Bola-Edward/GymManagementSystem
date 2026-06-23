7120cd7ce9ecff88eafc9a07b596b3bb4366bb40
bolaedward <bolaedward29@gmail.com>
2026-06-23 10:22:57 +0300

    Add Member entity and database configuration

---
+++ GymManagementSystem.DAL/Data/Configuration/MemberConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using GymManagementSystem.DAL.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymManagementSystem.DAL.Data.Configuration
+{
+    public class MemberConfiguration : UserConfiguration<Member>
+    {
+        public override void Configure(EntityTypeBuilder<Member> builder)
+        {
+            base.Configure(builder);
+
+            // configuration related to the Memeber entity can be added here if needed
+            builder.Property(p => p.Photo)
+                .HasMaxLength(1000);
+
+        }
+    }
+}
---
+++ GymManagementSystem.DAL/Models/Member.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymManagementSystem.DAL.Models
+{
+    public class Member : User
+    {
+        public string? Photo { get; set; }
+        public DateTime JoinDate { get; set; }
+
+
+    }
+}
