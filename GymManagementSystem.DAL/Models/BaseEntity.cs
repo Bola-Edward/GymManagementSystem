6714988e61554d78254785e204c240b2bce4da54
bolaedward <bolaedward29@gmail.com>
2026-06-23 09:50:37 +0300

    Add Base entity

---
+++ GymManagementSystem.DAL/Models/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymManagementSystem.DAL.Models
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        // use interceptor to set the CreatedAt, UpdatedAt, DeletedAt, IsDeleted properties automatically
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
