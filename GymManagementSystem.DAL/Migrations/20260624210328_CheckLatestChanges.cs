b8323c793e4db0e2219876b1a7d815eec8050754
bolaedward <bolaedward29@gmail.com>
2026-06-25 00:05:26 +0300

    Add Migration Check Latest Changes

---
+++ GymManagementSystem.DAL/Migrations/20260624210328_CheckLatestChanges.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GymManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class CheckLatestChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Email",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Phone",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Plans_Name",
+                table: "Plans");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bookings_MemberId_SessionId",
+                table: "Bookings");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Street",
+                table: "Users",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "JoinDate",
+                table: "Users",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Users",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BuildingNumber",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Notes",
+                table: "HealthRecords",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true,
+                filter: "[IsDeleted] = 0");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Phone",
+                table: "Users",
+                column: "Phone",
+                unique: true,
+                filter: "[IsDeleted] = 0");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Plans_Name",
+                table: "Plans",
+                column: "Name",
+                unique: true,
+                filter: "[IsDeleted] = 0");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_MemberId_SessionId",
+                table: "Bookings",
+                columns: new[] { "MemberId", "SessionId" },
+                unique: true,
+                filter: "[IsDeleted] = 0");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Email",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Phone",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Plans_Name",
+                table: "Plans");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bookings_MemberId_SessionId",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "Notes",
+                table: "HealthRecords");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Street",
+                table: "Users",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "JoinDate",
+                table: "Users",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateOnly),
+                oldType: "date",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Users",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BuildingNumber",
+                table: "Users",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Phone",
+                table: "Users",
+                column: "Phone",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Plans_Name",
+                table: "Plans",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_MemberId_SessionId",
+                table: "Bookings",
+                columns: new[] { "MemberId", "SessionId" },
+                unique: true);
+        }
+    }
+}
