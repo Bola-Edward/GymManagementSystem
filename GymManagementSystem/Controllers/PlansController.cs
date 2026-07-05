eea436c43404e32c87ca10ae9b3469991c55d2c6
bolaedward <bolaedward29@gmail.com>
2026-07-05 19:24:49 +0300

    Complete Plan presentation layer

---
+++ GymManagementSystem/Controllers/PlansController.cs
@@ -0,0 +1,88 @@
+﻿using GymManagementSystem.BusinessLogic.Services.Interfaces;
+using GymManagementSystem.BusinessLogic.ViewModels.PlanViewModels;
+using GymManagementSystem.DAL.Repositories;
+using GymManagementSystem.Data.Contexts;
+using GymManagementSystem.DataAccess.Repositories;
+using GymManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymManagementSystem.Controllers
+{
+    public class PlansController : Controller
+    {
+        private readonly IPlanService _planService;
+
+        public PlansController(IPlanService planService)
+        {
+            _planService = planService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var plans = await _planService.GetAllPlansAsync();
+
+            return View(plans);
+        }
+
+        public async Task<IActionResult> Details(int id, CancellationToken cancellationToken)
+        {
+            if (id <= 0)
+            {
+                return NotFound();
+            }
+
+            var plan = await _planService.GetPlanByIdAsync(id, cancellationToken);
+
+            if (plan is null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(plan);  // Views/Plans/Details.cshtml
+        }
+
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id, CancellationToken cancellationToken)
+        {
+            var plan = await _planService.GetPlanToUpdateAsync(id, cancellationToken);
+            if (plan is null)
+            {
+                TempData["ErrorMessage"] = "Plan cannot be edited (not found, inactive, or has active memberships).";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(plan);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, EditPlanViewModel model, CancellationToken cancellationToken)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var result = await _planService.UpdatePlanAsync(id, model, cancellationToken);
+            if (result)
+            {
+                TempData["SuccessMessage"] = "Plan updated successfully.";
+                return RedirectToAction(nameof(Index));
+            }
+            TempData["ErrorMessage"] = "Plan Failed To update";
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Activate(int id, CancellationToken ct)
+        {
+            var result = await _planService.ToggleActivationAsync(id, ct);
+            if (result)
+                TempData["SuccessMessage"] = "Plan status changed";
+            TempData["ErrorMessage"] = "Failed to Toggle Plan Status";
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
