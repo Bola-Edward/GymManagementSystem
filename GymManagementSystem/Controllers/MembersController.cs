a1c48839f5408e065c872d7c9c35e0405c5f9241
bolaedward <bolaedward29@gmail.com>
2026-07-05 15:55:34 +0300

    Add Member controller and views

--- GymManagementSystem/Controllers/MembersController.cs
+++ GymManagementSystem/Controllers/MembersController.cs
@@ -1,4 +1,5 @@
-﻿using GymManagementSystem.BusinessLogic.Services.Interfaces;
+﻿using GymManagementSystem.BLL.ViewModels.MemberViewModels;
+using GymManagementSystem.BusinessLogic.Services.Interfaces;
 using GymManagementSystem.BusinessLogic.ViewModels.MemberViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -37,6 +38,83 @@ namespace GymManagementSystem.PL.Controllers
             else
                 TempData["ErrorMessage"] = "Failed To Create Member";
 
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id, CancellationToken ct)
+        {
+            var member = await _memberService.GetDetailsAsync(id, ct);
+            if (member is null)
+            {
+                TempData["ErrorMessage"] = "Member not found.";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(member);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> HealthRecordDetails(int id, CancellationToken ct)
+        {
+            var record = await _memberService.GetHealthRecordAsync(id, ct);
+            if (record is null)
+            {
+                TempData["ErrorMessage"] = "Health record not found.";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(record);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id, CancellationToken cancellationToken)
+        {
+            var member = await _memberService.GetForEditAsync(id, cancellationToken);
+            if (member is null)
+            {
+                TempData["ErrorMessage"] = "Member not found.";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(member);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, EditMemberViewModel model, CancellationToken cancellationToken)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var result = await _memberService.UpdateAsync(id, model, cancellationToken);
+            if (result)
+            {
+                TempData["SuccessMessage"] = "Member updated successfully.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            TempData["ErrorMessage"] = "Failed To update Member";
+            return View(model);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id, CancellationToken ct)
+        {
+            var member = await _memberService.GetDetailsAsync(id, ct);
+            if (member is null)
+            {
+                TempData["ErrorMessage"] = "Member not found.";
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmed(int id, CancellationToken cancellationToken)
+        {
+            var result = await _memberService.RemoveAsync(id, cancellationToken);
+            if (result)
+                TempData["SuccessMessage"] = "Member deleted successfully.";
+            TempData["ErrorMessage"] = "Failed To delete Member";
             return RedirectToAction(nameof(Index));
         }
     }
