da9b79947fc0a6876ec7265ca159d797cd765d64
bolaedward <bolaedward29@gmail.com>
2026-07-12 12:27:29 +0300

    Add Membership view models

---
+++ GymManagementSystem.BLL/ViewModels/MembershipViewModels/CreateMemberShipViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymManagementSystem.BLL.ViewModels.MembershipViewModels
+{
+    public class CreateMemberShipViewModel
+    {
+        public int PlanId { get; set; }
+        public int MemberId { get; set; }
+        public DateTime? StartDate { get; set; }
+
+    }
+}
---
+++ GymManagementSystem.BLL/ViewModels/MembershipViewModels/MemberSelectListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymManagementSystem.BLL.ViewModels.MembershipViewModels
+{
+    public class MemberSelectListViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+    }
+}
