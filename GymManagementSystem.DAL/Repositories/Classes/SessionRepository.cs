7c06dc824b4d564d92ac043db5d7bb78a07a3881
bolaedward <bolaedward29@gmail.com>
2026-07-12 12:07:35 +0300

    Enhance Session repository

--- GymManagementSystem.DAL/Repositories/Classes/SessionRepository.cs
+++ GymManagementSystem.DAL/Repositories/Classes/SessionRepository.cs
@@ -1,16 +1,39 @@
 ﻿using GymManagementSystem.DAL.Models;
 using GymManagementSystem.DAL.Repositories.Interfaces;
 using GymManagementSystem.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace GymManagementSystem.DAL.Repositories.Classes
 {
     public class SessionRepository : Repository<Session>, ISessionRepository
     {
+        private readonly GymDbContext _dbContext;
+
         public SessionRepository(GymDbContext dbContext) : base(dbContext)
         {
+            _dbContext = dbContext;
         }
+        public async Task<IEnumerable<Session>> GetAllSessionsWithTrainerAndCategoryAsync(Expression<Func<Session, bool>>? predicate = null, CancellationToken cancellationToken = default)
+        {
+            IQueryable<Session> query = _dbContext.Sessions
+                .AsNoTracking()
+                .Include(s => s.Trainer)
+                .Include(s => s.Category);
+
+            if (predicate is not null) query = query.Where(predicate);
+
+            return await query.ToListAsync(cancellationToken);
+        }
+
+
+        public Task<int> GetCountOfBookedSlotsAsync(int sessionId, CancellationToken cancellationToken = default)
+            => _dbContext.Bookings.AsNoTracking().CountAsync(b => b.SessionId == sessionId, cancellationToken);
+
+        public Task<Session?> GetSessionWithTrainerAndCategoryAsync(int sessionId, CancellationToken cancellationToken = default)
+            => _dbContext.Sessions.AsNoTracking().Include(s => s.Trainer).Include(s => s.Category).FirstOrDefaultAsync(s => s.Id == sessionId, cancellationToken);
     }
 }
--- GymManagementSystem.DAL/Repositories/Interfaces/ISessionRepository.cs
+++ GymManagementSystem.DAL/Repositories/Interfaces/ISessionRepository.cs
@@ -1,11 +1,20 @@
 ﻿using GymManagementSystem.DAL.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace GymManagementSystem.DAL.Repositories.Interfaces
 {
     public interface ISessionRepository : IRepository<Session>
     {
+
+        Task<IEnumerable<Session>> GetAllSessionsWithTrainerAndCategoryAsync(
+      Expression<Func<Session, bool>>? predicate = null,
+      CancellationToken cancellationToken = default);
+
+        Task<Session?> GetSessionWithTrainerAndCategoryAsync(int sessionId, CancellationToken cancellationToken = default);
+
+        Task<int> GetCountOfBookedSlotsAsync(int sessionId, CancellationToken cancellationToken = default);
     }
 }
