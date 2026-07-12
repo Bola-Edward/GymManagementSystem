10070876863318afbb74d0d38c609488c5d6f9d0
bolaedward <bolaedward29@gmail.com>
2026-07-12 12:37:34 +0300

    Add Booking mapping profile

---
+++ GymManagementSystem.BLL/Mapping/BookingMappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using GymManagementSystem.BLL.ViewModels.BookingViewModels;
+using GymManagementSystem.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymManagementSystem.BLL.Mapping
+{
+    public class BookingMappingProfile : Profile
+    {
+        public BookingMappingProfile()
+        {
+
+            CreateMap<CreateBookingViewModel, Booking>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.Now));
+
+
+            CreateMap<Booking, MemberForSessionViewModel>()
+                .ForMember(dest => dest.MemberName, opt => opt.MapFrom(src => src.Member.Name))
+                .ForMember(dest => dest.BookingDate, opt => opt.MapFrom(src => src.CreatedAt.ToString("yyyy-MM-dd hh:mm tt")));
+        }
+    }
+}
