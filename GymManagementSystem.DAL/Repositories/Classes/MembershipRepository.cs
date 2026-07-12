cb180721a7f2f7ae383c887f48c2b312a5975fe2
bolaedward <bolaedward29@gmail.com>
2026-07-12 12:29:14 +0300

    Enhance Membership repository

--- GymManagementSystem.DAL/Repositories/Classes/MembershipRepository.cs
+++ GymManagementSystem.DAL/Repositories/Classes/MembershipRepository.cs
@@ -1,16 +1,33 @@
 ﻿using GymManagementSystem.DAL.Models;
 using GymManagementSystem.DAL.Repositories.Interfaces;
 using GymManagementSystem.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace GymManagementSystem.DAL.Repositories.Classes
 {
     public class MembershipRepository : Repository<MemberShip>, IMembershipRepository
     {
+        private readonly GymDbContext _dbContext;
+
         public MembershipRepository(GymDbContext dbContext) : base(dbContext)
         {
+            _dbContext = dbContext;
+        }
+
+
+
+        public async Task<List<MemberShip>> GetAllMembershipsWithMemberAndPlanAsync(Expression<Func<MemberShip, bool>>? predicate = null,
+           CancellationToken cancellationToken = default)
+        {
+            IQueryable<MemberShip> query = _dbContext.Memberships.AsNoTracking().Include(m => m.Plan).Include(m => m.Member);
+
+            if (predicate is not null) query = query.Where(predicate);
+
+            return await query.ToListAsync(cancellationToken);
         }
     }
 }
--- GymManagementSystem.DAL/Repositories/Interfaces/IMembershipRepository.cs
+++ GymManagementSystem.DAL/Repositories/Interfaces/IMembershipRepository.cs
@@ -1,11 +1,14 @@
 ﻿using GymManagementSystem.DAL.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace GymManagementSystem.DAL.Repositories.Interfaces
 {
     public interface IMembershipRepository : IRepository<MemberShip>
     {
+        Task<List<MemberShip>> GetAllMembershipsWithMemberAndPlanAsync(Expression<Func<MemberShip, bool>>? predicate = null, CancellationToken cancellationToken = default);
+
     }
 }
