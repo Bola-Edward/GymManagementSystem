ce9dd25e7cf443e714bb767c6c9cee50868ea4f7
bolaedward <bolaedward29@gmail.com>
2026-07-12 12:36:59 +0300

    Add Booking view models

---
+++ GymManagementSystem.BLL/ViewModels/BookingViewModels/CreateBookingViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymManagementSystem.BLL.ViewModels.BookingViewModels
+{
+    public class CreateBookingViewModel
+    {
+        public int MemberId { get; set; }
+        public int SessionId { get; set; }
+
+    }
+}
